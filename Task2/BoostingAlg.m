b4d892e220df5bba37ae369f101c5a1438e5b8d7
johntu <john.brynte@gmail.com>
2013-05-06 14:19:27 +0200

    Started working on the AdaBoost algorithm.

---
+++ Task2/BoostingAlg.m
@@ -0,0 +1,18 @@
+%%
+% Implements the AdaBoost algorithm.
+%
+% Fdata = 
+function Cparams = BoostingAlg(Fdata, NFdata, FTdata, T)
+
+% number of negative examples
+m = 
+
+% initialize the n weights
+w = 
+
+Cparams.alphas = 0;
+Cparams.Thetas = 0;
+Cparams.fmat = 0;
+Cparams.all_ftypes = 0;
+
+end
