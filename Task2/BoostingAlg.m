d8351970df382aba1a5a8625449e2ef8a9af5a32
johntu <john.brynte@gmail.com>
2013-05-11 13:56:27 +0200

    Fixed some errors.

--- Task2/BoostingAlg.m
+++ Task2/BoostingAlg.m
@@ -9,7 +9,11 @@
 function Cparams = BoostingAlg(Fdata, NFdata, FTdata, T)
 
 % all image data
-Idata = [Fdata.ii_ims; NFdata.ii_ims];
+% TODO
+Idata = [Fdata.ii_ims; NFdata.ii_ims];% .* FTdata.fmat;
+% feature vector labels
+labels = [ones(nf, 1); zeros(nnf, 1)];
+
 % number of faces
 nf = length(Fdata.fnums);
 % number of non-faces
@@ -18,18 +22,17 @@ nnf = length(NFdata.fnums);
 n = nf + nnf;
 % size of each feature vector
 N = size(Fdata.ii_ims, 2);
-% feature vector labels
-labels = [ones(1, nf) zeros(1, nnf)];
+
 % normalize the n weights so they sum up to one
-w(labels==0) = 1/(2*nnf);
-w(labels==1) = 1/(2*nf);
-w(:) = w(:)/sum(w(:));
+ws = zeros(size(labels));
+ws(labels==0) = 1/(2*nnf);
+ws(:) = ws(:)/sum(ws(:));
 
 % initialize the thetas of the strong classifier
-Cparams.Thetas = zeros(T, 2);
-Cparams.aplhas = zeros(T, 1);
+Cparams.Thetas = zeros(T, 3);
+Cparams.alphas = zeros(T, 1);
 
-for t = 0:T   
+for t = 1:T   
    % the error for each feature in the feature vectors
    %e = zeros(1, N);
    Err = Inf;
@@ -44,11 +47,11 @@ for t = 0:T
       
       % Calculate a weak classifier for the j:th feature of the i:th
       % feature vector
-      [theta, p, err] = LearnWeakClassifier(w, Idata, labels, j);
+      [theta, p, err] = LearnWeakClassifier(ws, Idata, labels, j);
       
       % Update the parameters if the error is a new minimum
       if err < Err
-          Cparams.Thetas(t) = [j, theta];
+          Cparams.Thetas(t, :) = [j, p, theta];
           Theta = theta;
           P = p;
           J = j;
@@ -59,7 +62,7 @@ for t = 0:T
    
    % Update the weights
    beta = err/(1 - err);
-   w(:) = w(:).*beta.^(1 - abs((Idata(:, J)*P < P*Theta) - labels(:)));
+   ws(:) = ws(:).*beta.^(1 - abs((Idata(:, J)*P < P*Theta) - labels(:)));
    Cparams.alphas(t) = log(1/beta);
 end
 
