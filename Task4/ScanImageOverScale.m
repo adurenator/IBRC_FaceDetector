e339248ca512dc99b9dd10baf3cf1c6069b0b7be
johntu <john.brynte@gmail.com>
2013-05-12 13:08:29 +0200

    Implemented ScanImageOverScale (not tested).

---
+++ Task4/ScanImageOverScale.m
@@ -0,0 +1,20 @@
+%%
+% Scans a given image in different scales to detect more faces.
+%
+% Cparams: Parameters used for the face detection.
+% im: The image to be scanned.
+% min_s: The minimum scale factor.
+% max_s: The maximum scale factor.
+% step_s: The number of scalings between the minimum and maximum scale
+%         factors.
+function dets = ScanImageOverScale(Cparams, im, min_s, max_s, step_s)
+
+% Define the set of scalings
+scale = min_s : step_s : max_s;
+
+dets = [];
+for s = scale
+    dets = [dets; ScanImageFixedSize(Cparams, resize(im, s))];
+end
+
+end
